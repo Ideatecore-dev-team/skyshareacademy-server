f1a648d27693c7850d69f56ea0a13046c8629915
WeissCurry <Maulanasykari@gmail.com>
2025-05-19 20:46:00 +0700

    FEAT : index.js, changing port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./src/api/app");
 
-app.listen(3000, () => {
-  console.log("app listening on port 3000!");
+app.listen(3002, () => {
+  console.log("app listening on port 3002!");
 });
