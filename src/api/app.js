12806af01a43577909a6e727e0f234cb8491527b
Mualana <122807476+WeissCurry@users.noreply.github.com>
2025-06-12 23:46:39 +0700

    FIX : Cors, ya allah

--- src/api/app.js
+++ src/api/app.js
@@ -3,11 +3,9 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
 
-// error middleware
 const ResponseError = require("../error/ResponseError");
 const errorMiddleware = require("../error/errorMiddlewae");
 
-// router
 const adminRoute = require("../api/admin/route");
 const articleRoute = require("../api/article/route");
 const partnerRoute = require("../api/partner/route");
@@ -22,18 +20,16 @@ const parentRoute = require("../api/parent/route");
 
 const app = express();
 
-// Konfigurasi CORS
 const allowedOrigins = [
   "https://dev.skyshareacademy.id",
   "https://skyshareacademy.id",
-  "http://localhost:5173", // Gunakan http:// untuk localhost jika tidak menggunakan https
-  "http://localhost:5174", // Gunakan http:// untuk localhost jika tidak menggunakan https
+  "http://localhost:5173",
+  "http://localhost:5174",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Izinkan permintaan tanpa origin (misalnya, permintaan dari Postman atau curl)
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
         const msg =
@@ -42,41 +38,20 @@ app.use(
       }
       return callback(null, true);
     },
-    credentials: true, // Penting jika Anda menggunakan cookie atau sesi
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-// Tambahan header CORS untuk memastikan
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// limit request from one IP address per second
-
-// delay
-// app.use(async (req, res, next) => {
-//     await new Promise((resolve) => setTimeout(resolve, 3000));
-//     next();
-// });
-
 app.get("/", (req, res) => {
   res.send("Hello World Sekai!");
 });
 
-// routes
 app.use(adminRoute);
 app.use(articleRoute);
 app.use(partnerRoute);
